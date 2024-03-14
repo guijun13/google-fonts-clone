7453c162d5fec7b1314ad530e803c9552d3adb97
guijun13 <guilhermejun13.grillo@gmail.com>
2024-03-14 00:41:04 -0300

    feat: style specific font page

--- src/views/FamilyView.vue
+++ src/views/FamilyView.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRoute, RouterLink } from 'vue-router';
 
 const route = useRoute();
 </script>
 
 <template>
-  <div>
-    <h1>{{ route.params.family }}</h1>
+  <div class="flex flex-col items-center gap-y-4">
+    <h1 class="text-[32px] text-center">{{ route.params.family }}</h1>
+    <RouterLink
+      class="w-fit text-center p-4 text-blue-500 border border-solid border-blue-400 rounded-full transtion hover:bg-blue-100 duration-300"
+      to="/"
+    >
+      Browse other fonts
+    </RouterLink>
   </div>
 </template>
